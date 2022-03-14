ac85d79cbcdb25e053e038eded24e3b808d901a6
NUishumb <125101@inbox.ru>
2022-03-14 17:16:58 +0700

    fixes

--- src/js/app.js
+++ src/js/app.js
@@ -2,9 +2,11 @@ import read from './reader';
 import json from './parser';
 
 export default class GameSavingsLoader {
-  static async load() {
-    const data = await read();
-    const value = await json(data);
-    return value;
+  static load() {
+    return new Promise((resolve) => {
+      read().then((data) => json(data)).then((data) => {
+        resolve(data);
+      });
+    });
   }
 }
